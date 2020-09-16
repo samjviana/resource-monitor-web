3cd7f0461595a873b9051723982805432b8c66a9
Maarsupial <sam.jviana@gmail.com>
2020-09-16 10:42:28 -0300

    tweaks

--- js/custom/main.js
+++ js/custom/main.js
@@ -1,6 +1,6 @@
-import * as rmsidebar from './components/rm-sidebar.js';
-import * as rmtabs from './components/rm-tabs.js';
-import * as rmcpucard from './components/rm-cpu-card.js';
-import * as rmgpucard from './components/rm-gpu-card.js';
-import * as rmramcard from './components/rm-ram-card.js';
-import * as rmstoragecard from './components/rm-storage-card.js';
+import './components/rm-sidebar.js';
+import './components/rm-tabs.js';
+import './components/rm-cpu-card.js';
+import './components/rm-gpu-card.js';
+import './components/rm-ram-card.js';
+import './components/rm-storage-card.js';
